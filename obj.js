cc13964e25239908e967d3ca4d7e0f632cc946ac
HEMALATHA01492 <hema01492@gmail.com>
2023-04-20 22:10:16 +0530

    Day3tasks

---
+++ obj.js
@@ -0,0 +1,27 @@
+let data={
+    Title:"RESUME",
+    Name:"HEMALATHA",
+    Mobile:"9962998110",
+    Address:[{
+            No:"84",
+            Street:"Nandas Midas City",
+            Post:"Athur",
+            City:"Chengalpattu",
+            Pincode:"603101"
+             }],
+    Education:[
+     {    
+ Type:"SSLC",
+            Percentage:"91"
+        },
+        {
+            Type:"HSC",
+            Percentage:"87"
+        },
+        { Type:"UG",
+            Percentage:"77"
+        }  ],
+    ProgrammingSkills:["C","C++"],
+    Experience:"Fresher"
+     }
+console.log(data);
